1eda3b489509e1e12ce110d67b1281edbb52465f
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-04-26 19:09:43 -0500

    Action with basic support for segregated checkAccess

---
+++ src/actions/Action.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tecnocen\roa\actions;
+
+class Action extends \yii\rest\Action;
+{
+    /**
+     * @param ?ActiveRecord $model
+     * @param array $params
+     * @throws \yii\web\HTTPException
+     */
+    public function checkAccess($model = null, array $params = [])
+    {
+        $this->controller->checkAccess($this->id, $model, $params);
+        if ($model->hasMethod('checkAccess')) {
+            $model->checkAccess($params);
+        }
+        if (isset($this->checkAccess)) {
+            call_user_func($this->checkAccess, $model, $params);
+        }
+    }
+}
