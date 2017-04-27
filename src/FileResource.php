53db03881dfb7179a42174f4b7a8d7c8738452dd
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-04-26 19:09:11 -0500

    interface to handle a file as a resource

---
+++ src/FileResource.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Interface to get a the information of a file associated to a model.
+ *
+ * @author Angel (Faryshta) Guevara <aguevara@alquimiadigital.mx>
+ */
+interface FileResource
+{
+    /**
+     * Full path for the file.
+     *
+     * @param string $ext the requested extension.
+     * @return string the full path for the file.
+     */
+    public function filePath($ext);
+
+    /**
+     * Name used to save the file upon download.
+     *
+     * @param string $ext the requested extension.
+     * @return ?string
+     */
+    public function fileName($ext);
+
+    /**
+     * Full path for the file.
+     *
+     * @param string $ext the requested extension.
+     * @return string 
+     */
+    public function fileMimeType($ext);
+}
