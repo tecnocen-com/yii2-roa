810ede23ddaab3b065cdf4b31b8d717a07e0c3ee
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-04-27 17:07:54 -0500

    soft delete action

---
+++ src/actions/SoftDelete.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace tecnocen\roa\actions;
+
+use Yii;
+
+/**
+ * Access and show s the content of a file on the browser or download it.
+ *
+ * @author Angel (Faryshta) Guevara <aguevara@alquimiadigital.mx>
+ */
+class SoftDelete extends Action
+{
+    /**
+     * Shows the file on the browser or download it after checking access.
+     *
+     * @param mixed $id the identifier value.
+     * @param string $ext the requested file extension.
+     */
+    public function run($id, $ext)
+    {
+        $this->checkAccess(
+            ($model = $this->findModel($id)),
+            Yii::$app->request->queryParams
+        );
+
+
+        if (false === $model->softDelete()) {
+            throw new ServerErrorHttpException('Failed to delete the object for unknown reason.');
+        }
+        Yii::$app->getResponse()->setStatusCode(204);
+    }
+}
