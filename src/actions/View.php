7d7f3b44d616b65301e157d6641d5a65b92b068f
Luis Campos <lcampos@solmipro.com>
2017-05-10 17:19:09 -0500

    Se modifica accion view

--- src/actions/View.php
+++ src/actions/View.php
@@ -3,33 +3,18 @@
 namespace tecnocen\roa\actions;
 
 use Yii;
-use yii\data\ActiveDataProvider;
 
 class View extends Action
 {
     /**
      * @return ActiveDataProvider
      */
-    public function run()
+    public function run($id)
     {
+        /* @var $model ActiveRecord */
+        $model = $this->findModel($id);
         $request = Yii::$app->getRequest();
-        $this->checkAccess(null, $request->getQueryParams());
-        return $this->prepareDataProvider();
-    }
-    /**
-     * Prepares the data provider that should return the requested collection of the models.
-     * @return ActiveDataProvider
-     */
-    protected function prepareDataProvider()
-    {
-        if ($this->prepareDataProvider !== null) {
-            return call_user_func($this->prepareDataProvider, $this);
-        }
-        /* @var $modelClass \yii\db\BaseActiveRecord */
-        $modelClass = $this->modelClass;
-        return Yii::createObject([
-            'class' => ActiveDataProvider::className(),
-            'query' => $modelClass::find(),
-        ]);
+        $this->checkAccess($model, $request->getQueryParams());
+        return $model;
     }
 }
