a721069108682001e756f9709e4481ef49f34db3
Marcos Jesús Chávez Vega <m-j.chavez.v@hotmail.com>
2017-08-19 01:08:17 -0500

    Expose headers (#16)
    Expose Pagination Headers

--- src/controllers/OAuth2Resource.php
+++ src/controllers/OAuth2Resource.php
@@ -321,7 +321,12 @@ class OAuth2Resource extends \yii\rest\ActiveController
             'Access-Control-Request-Headers' => ['*'],
             'Access-Control-Allow-Credentials' => null,
             'Access-Control-Max-Age' => 86400,
-            'Access-Control-Expose-Headers' => [],
+            'Access-Control-Expose-Headers' => [
+                'x-pagination-current-page', 
+                'x-pagination-page-count',
+                'x-pagination-per-page',
+                'x-pagination-total-count'
+            ],
         ];
     }
 
