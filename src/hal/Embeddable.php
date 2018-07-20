2f909e0ea13b1256f9d633b9f63fb9be58536507
Angel Guevara <angeldelcaos@gmail.com>
2018-07-19 22:45:05 -0500

    include nested expand logic from yii2 2.0.14

--- src/hal/Embeddable.php
+++ src/hal/Embeddable.php
@@ -13,23 +13,6 @@ interface Embeddable extends \yii\base\Arrayable
     const EMBEDDED_PROPERTY = '_embedded';
 
     /**
-     * Converts the model into an array.
-     *
-     * @param array $fields the fields being requested. If empty, all fields as
-     * specified by [[fields()]] will be returned.
-     * @param array $expand the additional fields being requested for exporting.
-     * Only fields declared in [[extraFields()]] will be considered.
-     * @param bool $recursive whether to recursively return array representation
-     * of embedded objects.
-     * @return array the array representation of the object
-     */
-    public function toArray(
-        array $fields = [],
-        array $expand = [],
-        $recursive = true
-    );
-
-    /**
      * @return string property which will contain all the expanded parameters.
      */
     public function getExpandEnvelope();
