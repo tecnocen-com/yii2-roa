fbcfea6601298d24329f8adc3d9c32969de96752
Carlos Llamosas <carlos@tecnocen.com>
2018-08-12 13:47:45 -0500

    Add comment

--- tests/_app/fixtures/EmployeeFixture.php
+++ tests/_app/fixtures/EmployeeFixture.php
@@ -4,11 +4,24 @@ namespace app\fixtures;
 
 use app\models\Employee;
 
+/**
+ * Fixture to load default employees.
+ */
 class EmployeeFixture extends \yii\test\ActiveFixture
 {
+
+    /**
+     * @inheritdoc
+     */	
 	public $depends = ['app\fixtures\ShopFixture'];
 
+    /**
+     * @inheritdoc
+     */
     public $modelClass = Employee::class;
-    
+
+    /**
+     * @inheritdoc
+     */    
     public $dataFile = __DIR__ . '/data/employee.php';
 }
